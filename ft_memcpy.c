ddafe76e62441a81c521424e3feb504ba66ce47e
Fatemeh Yousefi <fyousefi@f1r1s19.codam.nl>
2025-10-10 14:51:15 +0200

    ft_memcpy function added and norminette checked

---
+++ ft_memcpy.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyousefi <fyousefi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/18 13:37:42 by kube              #+#    #+#             */
+/*   Updated: 2025/10/10 14:50:14 by fyousefi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	const unsigned char	*s;
+	unsigned char		*d;
+
+	s = src;
+	d = dst;
+	while (n > 0)
+	{
+		*d = *s;
+		d ++;
+		s ++;
+		n --;
+	}
+	return (dst);
+}
+/* int	main()
+{
+	char src[] = "Hello";
+	char dest[20];
+	ft_memcpy(dest, src, 3);
+	printf("%s", dest);
+	return 0;
+} */
