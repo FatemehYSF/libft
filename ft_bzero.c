ff1babfee4d607a51aba4811ff55c846f549ae86
Fatemeh Yousefi <fyousefi@f1r1s19.codam.nl>
2025-10-10 13:50:00 +0200

    ft_bzero added and norminette checked

---
+++ ft_bzero.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyousefi <fyousefi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/18 13:37:42 by kube              #+#    #+#             */
+/*   Updated: 2025/10/10 13:47:03 by fyousefi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*p;
+
+	p = s;
+	while (n > 0)
+	{
+		*p = '\0';
+		p ++;
+		n --;
+	}
+}
+
+/* int main()
+{
+    char box[10];
+    ft_bzero(box, sizeof(box));
+    for (int i = 0; i < 10; i++) {
+        printf("%c ", box[i]);
+    }
+    return 0;
+} */
