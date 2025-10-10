b7bea7fd5a3ec68fbd40d50b7fe377dd30f6f714
Fatemeh Yousefi <fyousefi@f1r1s19.codam.nl>
2025-10-10 11:14:44 +0200

    memset function- block of memory with a specific value

---
+++ ft_memset.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyousefi <fyousefi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/18 13:37:42 by kube              #+#    #+#             */
+/*   Updated: 2025/10/10 11:12:41 by fyousefi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	*p;
+	unsigned char	*start;
+
+	p = (unsigned char *)s;
+	start = p;
+	while (n > 0)
+	{
+		*p = c;
+		p ++;
+		n --;
+	}
+	return (start);
+}
+
+/* int main()
+{
+    char box[10];
+    ft_memset(box, 'A', sizeof(box));
+    for (int i = 0; i < 10; i++) {
+        printf("%c ", box[i]);
+    }
+    return 0;
+} */
