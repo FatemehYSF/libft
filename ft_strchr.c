c6ce78e9cf7142df3892014f4140675730a6f9c4
Fatemeh <fatemeh.yousefi.c71@gmail.com>
2025-10-16 13:16:00 +0200

    ft_strcht added

---
+++ ft_strchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyousefi <fyousefi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/18 13:37:42 by kube              #+#    #+#             */
+/*   Updated: 2025/10/10 11:12:41 by fyousefi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (*s == c)
+		return ((char *)s);
+	return (NULL);
+}
+
+// int main()
+// {
+// 	char s[7] = "Fatemeh";
+// 	printf("%s", ft_strchr(s , 'a'));
+// }
