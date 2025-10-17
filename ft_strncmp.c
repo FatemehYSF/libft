ad7659452d6bcb469451f5dc55f69346b537c54a
Fatemeh <fatemeh.yousefi.c71@gmail.com>
2025-10-17 15:02:36 +0200

    ft_strncmp added

---
+++ ft_strncmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyousefi <fyousefi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/18 13:37:42 by kube              #+#    #+#             */
+/*   Updated: 2025/10/10 11:12:41 by fyousefi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n && (s1[i] == s2[i]))
+	{
+		i ++;
+	}
+	if (i == n)
+		return (0);
+	else
+		return (s1[i] - s2[i]);
+}
+int main()
+{
+	printf("%d\n" , ft_strncmp("Fatemeh", "Fateh" , 4));
+	printf("%d" , ft_strncmp("Fatemeh", "Fateh" , 5));
+return 0;
+}
---
+++ ft_strrchr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyousefi <fyousefi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/18 13:37:42 by kube              #+#    #+#             */
+/*   Updated: 2025/10/10 11:12:41 by fyousefi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = -1;
+	while (s[i])
+	{
+		if (s[i] == c)
+			j =i;
+		i ++;
+	}
+	if (j >= 0)
+		return ((char *)&s[j]);
+	else if ((char)c == '\0')
+    	return ((char *)&s[i]);
+	else
+	return (NULL);
+}
+
+// int main()
+// {
+// 	char s[] = "Fatemeh";
+// 	printf("%s\n", ft_strrchr(s, 'e'));
+// printf("%s\n", ft_strrchr(s, 'F'));
+// printf("%s\n", ft_strrchr(s, 'z'));
+// printf("%s\n", ft_strrchr(s, '\0'));
+// 	return 0;
+// }
