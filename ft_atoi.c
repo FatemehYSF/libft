e6bad5249d3f76de8ae6c7d8ede31db3cac4ebd4
Fatemeh <fatemeh.yousefi.c71@gmail.com>
2025-10-21 10:44:18 +0200

    ft_atoi.c added

---
+++ ft_atoi.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyousefi <fyousefi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/18 13:37:42 by kube              #+#    #+#             */
+/*   Updated: 2025/10/10 13:47:03 by fyousefi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_isdigit(int n)
+{
+	return (n >= '0' && n <= '9');
+}
+
+int	ft_isspace(int c)
+{
+	return ((c >= 9 && c <= 13) || c == ' ');
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	sign;
+	int	i;
+	int	result;
+	int	digit;
+
+	sign = 1;
+	i = 0;
+	result = 0;
+	while (ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		digit = nptr[i] - '0';
+		result = result * 10 + digit;
+		i++;
+	}
+	return (result * sign);
+}
+/* int main(void)
+{
+    const char *tests[] = {
+        "42",
+        "   123",
+        "   -42",
+        "+99",
+        "--56",
+        "  +00123abc",
+        "abc123",
+        "   -0000042",
+        "",
+        "   +0",
+        NULL
+    };
+
+    int i = 0;
+    while (tests[i])
+    {
+        printf("Input: \"%s\"  →  Output: %d\n", tests[i], ft_atoi(tests[i]));
+        i++;
+    }
+
+    return 0;
+} */
