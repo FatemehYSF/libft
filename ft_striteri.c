3519805078972a9ab2e516231dd9c8213d92b100
Fatemeh <fatemeh.yousefi.c71@gmail.com>
2025-10-24 17:46:26 +0200

    ft_striteri.c added and .h edited

---
+++ ft_striteri.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fyousefi <fyousefi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/11/18 13:37:42 by kube              #+#    #+#             */
+/*   Updated: 2025/10/22 12:05:31 by fyousefi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	unsigned	int i;
+
+	if (!s || !f)
+		return;
+	i= 0;
+	while (s[i])
+	{
+		f(i, &s[i]);
+		i++;
+	}
+}
+/* void to_upper(unsigned int i, char *c)
+{
+    (void)i;
+    if (*c >= 'a' && *c <= 'z')
+        *c -= 32;
+}
+
+int main(void)
+{
+    char str[] = "fatemeh";
+    ft_striteri(str, to_upper);
+    printf("%s\n", str);
+    return 0;
+} */
